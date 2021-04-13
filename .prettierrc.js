15fa104943fc9512316d8c2235c4ca20e444e263
Greg Butt <greg.butt@scaledagile.com>
2021-04-12 19:13:06 -0600

    setup sfdx environment

---
+++ .prettierrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+    printWidth: 100,
+    tabWidth: 4,
+    trailingComma: 'es5',
+    singleQuote: true,
+    overrides: [
+        {
+            files: '*.cmp',
+            options: {
+                printWidth: 160,
+                tabWidth: 2,
+                trailingComma: 'none',
+            },
+        },
+        {
+            files: '*.cls',
+            options: {
+                printWidth: 120,
+            },
+        },
+        {
+            files: '*.css',
+            options: {
+                singleQuote: false,
+            },
+        },
+        {
+            files: '**/lwc/**/*.html',
+            options: {
+                printWidth: 160,
+                tabWidth: 2,
+                parser: 'lwc',
+            },
+        },
+        {
+            files: '*.{cmp,page,component}',
+            options: { parser: 'html' },
+        },
+    ],
+};
